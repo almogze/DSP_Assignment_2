6d8c3cab1eb4663d0a82cc9523e85a99eb16658f
raz-mon <razmon@post.bgu.ac.il>
2021-12-15 19:56:31 +0200

    src + pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,99 @@
+// import org.apache.hadoop.mapreduce.TestMapCollection.StepFactory;
+
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClientBuilder;
+import com.amazonaws.services.elasticmapreduce.model.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
 public class Main {
+    public static AWSCredentialsProvider credentialsProvider;
+    public static AmazonS3 S3;
+    public static AmazonEC2 ec2;
+    public static AmazonElasticMapReduce emr;
+
+    public static void main(String[] args){
+
+/*
+Meni's code (example)
+        AWSCredentials credentials = new PropertiesCredentials();
+        AmazonElasticMapReduce mapReduce = new AmazonElasticMapReduceClient(credentials);
+        HadoopJarStepConfig hadoopJarStep = new HadoopJarStepConfig()
+                .withJar("s3n://yourbucket/yourfile.jar") // This should be a full map reduce application.
+                .withMainClass("some.pack.MainClass")
+                .withArgs("s3n://yourbucket/input/", "s3n://yourbucket/output/");
+        StepConfig stepConfig = new StepConfig()
+                .withName("stepname")
+                .withHadoopJarStep(hadoopJarStep)
+                .withActionOnFailure("TERMINATE_JOB_FLOW");
+        JobFlowInstancesConfig instances = new JobFlowInstancesConfig()
+                .withInstanceCount(2)
+                .withMasterInstanceType(InstanceType.M4Large.toString())
+                .withSlaveInstanceType(InstanceType.M4Large.toString())
+                .withHadoopVersion("2.6.0").withEc2KeyName("yourkey")
+                .withKeepJobFlowAliveWhenNoSteps(false)
+                .withPlacement(new PlacementType("us-east-1a"));
+        RunJobFlowRequest runFlowRequest = new RunJobFlowRequest()
+                .withName("jobname")
+                .withInstances(instances)
+                .withSteps(stepConfig)
+                .withLogUri("s3n://yourbucket/logs/");
+        RunJobFlowResult runJobFlowResult = mapReduce.runJobFlow(runFlowRequest);
+        String jobFlowId = runJobFlowResult.getJobFlowId();
+        System.out.println("Ran job flow with id: " + jobFlowId);
+ */
+
+
+
+
+
+
+
+
+        credentialsProvider =new EnvironmentVariableCredentialsProvider();
+
+
+        S3 = AmazonS3ClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion("us-west-2")
+                .build();
+        //delete the output file if it exist
+        ObjectListing objects = S3.listObjects("ass2razalmog1122", "outputAssignment2");
+        for (S3ObjectSummary s3ObjectSummary : objects.getObjectSummaries()) {
+            S3.deleteObject("assignment2dspmor", s3ObjectSummary.getKey());
+        }
+
+
+        ec2 = AmazonEC2ClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion("us-west-2")
+                .build();
+        System.out.println("creating a emr");
+        emr= AmazonElasticMapReduceClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion("us-west-2")
+                .build();
+
+        System.out.println( emr.listClusters());
+
+
+    }
+
 }
